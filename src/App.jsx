9abd4157ad6d865abac2d326dbcc5361e13d7c48
shandie <shancarl.belgica.3@gmail.com>
2022-12-01 16:28:02 +0800

    App creation

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='text-green-700 text-center font-bold text-5xl'>
+        Test App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
