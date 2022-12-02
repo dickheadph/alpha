ab12b0f678a28966f51bd8f1829de4e58ca34d8a
shanegel <48678135+shanegel@users.noreply.github.com>
2022-12-02 09:43:31 +0800

    Updated and Renamed App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className='App'>
       <h1 className='text-green-700 text-center font-bold text-5xl'>
-        Final Test
+        Renamed Test
       </h1>
     </div>
   );
